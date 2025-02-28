e2414f9f6240aa659ba2b316c303d115b0d502c5
alowrrany02 <130370163+alowrrany02@users.noreply.github.com>
2025-02-27 22:43:29 -0300

    Up

--- devclub-cadastro-usuarios/src/pages/Home/index.jsx
+++ devclub-cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,19 +1,53 @@
 import { useState } from 'react'
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const users = [
+    {
+      id:'2343fgd',
+      name: 'Rodolfo',
+      age: 23,
+      email:'shjsc@gmail.com'
+    },
+    {
+      id:'23fd43fgd',
+      name: 'Maria',
+      age: 33,
+      email:'scvsdf@gmail.com'
+    },
+    {
+      id:'2390d43fgd',
+      name: 'Joao',
+      age: 53,
+      email:'jagad@gmail.com'
+    }
+]
 
   return (
     <>
       <div className="container">
         <form>
             <h1>Cadastro de Formulário</h1>
-            <input name='nome' type='text'/>
-            <input name='idade' type='number' />
-            <input name='email' type='email' />
+            <input placeholder='Nome' name='nome' type='text'/>
+            <input placeholder='Idade' name='idade' type='number' />
+            <input placeholder='Email' name='email' type='email' />
             <button type='button'>Cadastrar</button>
         </form>
+        {users.map((user) => (
+
+        <div key={user.id}>
+          <div>
+            <p>Nome:{user.name}</p>
+            <p>Idade:{user.idade}</p>
+            <p>Email:{user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+        </div>
+          
+        ))}
       </div>
     </>
   )
